cf4db9d41be504125da8453bce2d6fa1f95e1c3d
nkx2ca <nkx2ca@gmail.com>
2026-08-18 22:24:48 +0900

    Fix for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nkx2ca.github.io',
+  base: '/Sandankyo-Hotel-Nampo',
   i18n: {
     locales: ["ja", "en"],
     defaultLocale: "ja",
