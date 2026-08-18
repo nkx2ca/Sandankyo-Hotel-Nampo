c8f4dc56b204027fac511c98ad882c30545905ec
nkx2ca <nkx2ca@gmail.com>
2026-08-18 21:25:43 +0900

    Update Top, Navigation, Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nkx2ca.github.io',
   i18n: {
     locales: ["ja", "en"],
     defaultLocale: "ja",
     routing: {
-      prefixDefaultLocale: true,
+      prefixDefaultLocale: false,
       redirectToDefaultLocale: true,
     },
   },
