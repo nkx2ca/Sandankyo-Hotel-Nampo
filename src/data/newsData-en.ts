132e6176216107b3dd16817a23dc782a430934b3
nkx2ca <nkx2ca@gmail.com>
2026-09-24 23:02:58 +0900

    Fix some page

--- src/data/newsData-en.ts
+++ src/data/newsData-en.ts
@@ -4,32 +4,10 @@
 // 2. Add import and newsArticles array below
 
 import article1 from './news-en/1';
-import article2 from './news-en/2';
-import article3 from './news-en/3';
-import article4 from './news-en/4';
-import article5 from './news-en/5';
-import article6 from './news-en/6';
-import article7 from './news-en/7';
-import article8 from './news-en/8';
-import article9 from './news-en/9';
-import article10 from './news-en/10';
-import article11 from './news-en/11';
-import article12 from './news-en/12';
 
 // All articles array
 export const newsArticles = [
   article1,
-  article2,
-  article3,
-  article4,
-  article5,
-  article6,
-  article7,
-  article8,
-  article9,
-  article10,
-  article11,
-  article12,
 ];
 
 // For list display: extract only title, date, and category
